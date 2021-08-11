d8aa2a28e591ca47ca1e21de6cf36fb96e97edca
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-08-11 17:21:11 -0400

    Connecting node with mongodb

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,18 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('/search', (req, res) => {
+    res.render('search',{
+        searchArr: ['apples', 'bannanas', 'pies']
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
