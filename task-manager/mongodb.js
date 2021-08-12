eaceaf1fd321db7d147282f5016272a14c76f733
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-08-12 18:38:32 -0400

    Mongodb CRUD operations

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,8 +1,11 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+const databaseName = 'task-manager' 
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true  }, (error, client) => {
     if(error){
@@ -10,9 +13,45 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     }
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Maleeha',
-        age: 22
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID("6115498ae947ec14a07bf88d")
+    // }, {
+    //     $set: {
+    //         name: 'Jessie'
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // db.collection('task').updateMany(
+    //     { completed: false },
+    //     {
+    //         $set: { completed: true }
+    //     }
+    // ).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log("An error has occured.")
+    // })
+
+    // db.collection('users').deleteMany({
+    //     name: 'Maria'
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    db.collection('users').deleteOne(
+        { name: 'Jessie' }
+    ).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
+
+
 
 })
